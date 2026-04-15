12a8689e3ba22b26f8301322550c7caf014de765
R4X05Z3R0 <razeroeuw@gmail.com>
2026-04-15 17:56:21 +0200

    Added some endpoints to controller for testing

--- src/main/java/com/example/wishlist/controller/UserController.java
+++ src/main/java/com/example/wishlist/controller/UserController.java
@@ -1,11 +1,27 @@
 package com.example.wishlist.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.wishlist.model.User;
+import com.example.wishlist.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/shootingstar/users")
 public class UserController {
+    private final UserService service;
 
+    public UserController(UserService service){
+        this.service = service;
+    }
 
+    @PostMapping()
+    public int createUser(@RequestBody User user){
+        return service.createUser(user);
+    }
+
+    @GetMapping("/list")
+    public List<User>getUsers(){
+        return service.getAllUsers();
+    }
 }
